df2a995a8f72df9400d755c074280185d24c66d7
waitrevor <trevorw5@student.byu.edu>
2022-09-06 10:48:15 -0600

    Finished questions 1-4

--- StandardLibrary/standard_library.py
+++ StandardLibrary/standard_library.py
@@ -1,8 +1,14 @@
+import calculator
+import box
+from itertools import combinations
+import random
+import time
+
 # standard_library.py
 """Python Essentials: The Standard Library.
-<Name>
-<Class>
-<Date>
+<Name> Trevor Wai
+<Class> Section 2
+<Date> 9/6/22
 """
 
 
@@ -11,7 +17,8 @@ def prob1(L):
     """Return the minimum, maximum, and average of the entries of L
     (in that order, separated by a comma).
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    #returns min max and average of elements from list L
+    return min(L), max(L), sum(L)/len(L)
 
 
 # Problem 2
@@ -19,7 +26,33 @@ def prob2():
     """Determine which Python objects are mutable and which are immutable.
     Test integers, strings, lists, tuples, and sets. Print your results.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    
+    a = 1
+    b = a
+    a += 1
+    print(a == b)
+    
+    a = 'Hello World!'
+    b = a
+    a += ' Goodbye World!'
+    print(a == b)
+
+    a = [1,2,3]
+    b = a
+    a.append(4)
+    print(a == b)
+
+    a = (4,5,6)
+    b = a
+    a += (1, )
+    print(a == b)
+    
+    a = {1,2,3}
+    b = a
+    a.add(4)
+    print(a == b)
+
+    print("Mutable: lists, sets Immutable: int, string, tuple")
 
 
 # Problem 3
@@ -34,7 +67,10 @@ def hypot(a, b):
     Returns:
         The length of the triangle's hypotenuse.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+
+    hyp = calculator.sqrt(calculator.sum(calculator.prod(a,a), calculator.prod(b,b)))
+
+    return hyp
 
 
 # Problem 4
@@ -47,10 +83,24 @@ def power_set(A):
     Returns:
         (list(sets)): The power set of A as a list of sets.
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    L = []
 
+    for i in range(len(A) + 1):
+        L += [set(j) for j in list(combinations(A,i))]
+
+    return L
 
 # Problem 5: Implement shut the box.
 def shut_the_box(player, timelimit):
     """Play a single game of shut the box."""
-    raise NotImplementedError("Problem 5 Incomplete")
+    
+    numbers_left = [1,2,3,4,5,6,7,8,9]
+    time_left = timelimit
+    while time_left > 0:
+        sum = random.randint(2,12)
+
+
+        if numbers_left == []:
+            print("Win") 
+            break  
+
