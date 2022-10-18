6216c7586a4517aa857b772505bf51007d83471a
waitrevor <trevorw5@student.byu.edu>
2022-10-18 10:22:46 -0600

    Finished Problem 1 - 3

--- QR_Decomposition/qr_decomposition.py
+++ QR_Decomposition/qr_decomposition.py
@@ -1,10 +1,11 @@
 # qr_decomposition.py
 """Volume 1: The QR Decomposition.
-<Name>
-<Class>
-<Date>
+<Name> Trevor Wai
+<Class> Section
+<Date> 10/18/22
 """
-
+import numpy as np
+import scipy.linalg as la
 
 # Problem 1
 def qr_gram_schmidt(A):
@@ -17,7 +18,17 @@ def qr_gram_schmidt(A):
         Q ((m,n) ndarray): An orthonormal matrix.
         R ((n,n) ndarray): An upper triangular matrix.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    m, n = A.shape
+    Q = A.copy().astype('float64')
+    R = np.zeros((n,n))
+    for i in range(n):
+        R[i,i] = la.norm(Q[:,i])
+        Q[:,i] = Q[:,i] / R[i,i]
+        for j in range(i+1, n):
+            R[i,j] = np.dot((Q[:,j].T), Q[:,i])
+            Q[:,j] = Q[:,j] - (R[i,j] * Q[:,i])
+
+    return Q, R
 
 
 # Problem 2
@@ -31,7 +42,7 @@ def abs_det(A):
     Returns:
         (float) the absolute value of the determinant of A.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    return abs(np.prod(np.diag(qr_gram_schmidt(A)[1])))
 
 
 # Problem 3
@@ -45,7 +56,19 @@ def solve(A, b):
     Returns:
         x ((n, ) ndarray): The solution to the system Ax = b.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    #Initialize Variables
+    m,n = np.shape(A)
+    Q,R = qr_gram_schmidt(A)
+    x = np.zeros(n)
+
+    #Solves for y
+    y = np.transpose(Q) @ b
+
+    #Uses y to solve for x
+    for j in reversed(range(n)):
+        x[j] = (y[j] - (R[j,:] @ x)) / R[j,j]
+
+    return x
 
 
 # Problem 4
@@ -75,3 +98,11 @@ def hessenberg(A):
         Q ((n,n) ndarray): An orthonormal matrix.
     """
     raise NotImplementedError("Problem 5 Incomplete")
+
+
+#Testing
+A = np.random.random((4,4))
+b = np.random.random(4)
+Q = qr_gram_schmidt(A)[0]
+R = qr_gram_schmidt(A)[1]
+print(A @ solve(A,b) == b)
